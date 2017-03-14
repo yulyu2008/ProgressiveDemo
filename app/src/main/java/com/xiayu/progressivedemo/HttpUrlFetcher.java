d7017f04dddccd83f256f751531f942c4fc25a5c
luoxiayu@163.com <475431737@qq.com>
2017-03-14 19:47:06 +0800

    change HttpUrlFetcher
    change HttpUrlFetcher

--- app/src/main/java/com/xiayu/progressivedemo/HttpUrlFetcher.java
+++ app/src/main/java/com/xiayu/progressivedemo/HttpUrlFetcher.java
@@ -9,14 +9,16 @@ import java.net.URL;
  * A DataFetcher that retrieves an {@link InputStream} for a Url.
  */
 public class HttpUrlFetcher {
-    private static final String TAG               = "HttpUrlFetcher";
+    private static final String TAG = "HttpUrlFetcher";
 
 
-    private          HttpURLConnection urlConnection;
-    private int size;
-    public int getSize(){
+    private HttpURLConnection urlConnection;
+    private int               size;
+
+    public int getSize() {
         return size;
     }
+
     public InputStream loadData(URL url)
             throws IOException {
         urlConnection = (HttpURLConnection) url.openConnection();
